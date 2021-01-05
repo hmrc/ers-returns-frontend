d0fe0d18b9867b58ac4a4e6a572ca776785dbd96
DannyPennington97 <63240616+DannyPennington97@users.noreply.github.com>
2021-01-05 14:41:29 +0000

    [DP][DDCE-1284] Copyright year updates

--- test/controllers/auth/AuthFunctionalitySpec.scala
+++ test/controllers/auth/AuthFunctionalitySpec.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright 2020 HM Revenue & Customs
+ * Copyright 2021 HM Revenue & Customs
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -30,7 +30,6 @@ import uk.gov.hmrc.domain.EmpRef
 import uk.gov.hmrc.play.test.UnitSpec
 import utils.ErsTestHelper
 
-import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
 
 class AuthFunctionalitySpec extends UnitSpec with ErsTestHelper with DefaultAwaitTimeout {
