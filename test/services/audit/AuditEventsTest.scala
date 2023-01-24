a606067756e66bfcf4211648d71eea3230b3d74f
WozCapGem <63247226+WozCapGem@users.noreply.github.com>
2023-01-24 14:04:30 +0000

    DDCE-3290, AuditEvent test

--- test/services/audit/AuditEventsTest.scala
+++ test/services/audit/AuditEventsTest.scala
@@ -18,8 +18,9 @@ package services.audit
 
 import models.{ErsMetaData, SchemeInfo}
 import org.joda.time.DateTime
+import org.mockito.ArgumentCaptor
 import org.mockito.ArgumentMatchers.any
-import org.mockito.Mockito.when
+import org.mockito.Mockito.{times, verify, when}
 import org.scalatest.OptionValues
 import org.scalatest.concurrent.ScalaFutures
 import org.scalatest.matchers.should.Matchers
@@ -84,6 +85,34 @@ class AuditEventsTest extends AnyWordSpecLike with Matchers with OptionValues wi
 				"bundleRed" -> "bundle"
 			)
 			testAuditEvent.eventMap(rsc, "bundle") shouldBe eventMap
+		}
+	}
+
+	"The auditRunTimeError DataEvent" should {
+		class TestException(message: String) extends Throwable(message)
+
+		"include the 'CheckingServiceFileProcessingError' auditType" in {
+			val mockAuditConnector: DefaultAuditConnector = mock[DefaultAuditConnector]
+			val testAuditEvent = new AuditEvents(mockAuditConnector)
+			val eventCaptor = ArgumentCaptor.forClass(classOf[DataEvent])
+			val testException: TestException = new TestException("testErrorMessage")
+			val expectedDataEvent: DataEvent = dataEvent.copy(
+				auditType = "RunTimeError",
+				detail = Map(
+					"ErrorMessage" -> "testErrorMessage",
+					"Context" -> "testContextInfo",
+					"sheetName" -> "testSheetName",
+					"StackTrace" -> testException.getStackTrace.toString
+				)
+			)
+
+			testAuditEvent.auditRunTimeError(testException, "testContextInfo", rsc, "testBundle")
+			verify(mockAuditConnector, times(1)).sendEvent(eventCaptor.capture())(any(), any())
+			expectedDataEvent.auditSource shouldBe eventCaptor.getValue.asInstanceOf[DataEvent].auditSource
+			expectedDataEvent.auditType shouldBe eventCaptor.getValue.asInstanceOf[DataEvent].auditType
+			expectedDataEvent.detail.head shouldBe eventCaptor.getValue.asInstanceOf[DataEvent].detail.head
+			expectedDataEvent.detail.take(1) shouldBe eventCaptor.getValue.asInstanceOf[DataEvent].detail.take(1)
+			expectedDataEvent.detail.take(2) shouldBe eventCaptor.getValue.asInstanceOf[DataEvent].detail.take(2)
 		}
 	}
 }
