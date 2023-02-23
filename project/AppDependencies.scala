aeb7c7932c6c891989f62df454d9790dbe567fb3
garytate123 <garytatelufc@gmail.com>
2023-02-23 16:25:40 +0000

    address platops-pr-bots comments

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -37,15 +37,15 @@ object AppDependencies {
   object Test {
     def apply(): Seq[ModuleID] = new TestDependencies {
       override lazy val test = Seq(
-        "org.scalatest"          %% "scalatest"               % "3.2.12"                      % scope,
-        "org.scalatestplus"      %% "mockito-3-4"             % "3.2.10.0"                    % scope,
-        "org.scalatestplus.play" %% "scalatestplus-play"      % "5.1.0"                       % scope,
-        "com.vladsch.flexmark"   %  "flexmark-all"            % "0.62.2"                      % scope,
-        "org.pegdown"            %  "pegdown"                 % "1.6.0"                       % scope,
-        "org.jsoup"              %  "jsoup"                   % "1.15.2"                      % scope,
-        "com.typesafe.play"      %% "play-test"               % PlayVersion.current           % scope,
-        "com.github.tomakehurst" %  "wiremock-standalone"     % "2.27.2"                      % scope,
-        "uk.gov.hmrc"            %% "bootstrap-test-play-28"  % bootstrapFrontendPlay28Version
+        "org.scalatest"          %% "scalatest"               % "3.2.12"                        % scope,
+        "org.scalatestplus"      %% "mockito-3-4"             % "3.2.10.0"                      % scope,
+        "org.scalatestplus.play" %% "scalatestplus-play"      % "5.1.0"                         % scope,
+        "com.vladsch.flexmark"   %  "flexmark-all"            % "0.62.2"                        % scope,
+        "org.pegdown"            %  "pegdown"                 % "1.6.0"                         % scope,
+        "org.jsoup"              %  "jsoup"                   % "1.15.2"                        % scope,
+        "com.typesafe.play"      %% "play-test"               % PlayVersion.current             % scope,
+        "com.github.tomakehurst" %  "wiremock-standalone"     % "2.27.2"                        % scope,
+        "uk.gov.hmrc"            %% "bootstrap-test-play-28"  % bootstrapFrontendPlay28Version  % scope,
       )
     }.test
   }
