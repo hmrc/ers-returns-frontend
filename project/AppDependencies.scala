f4a9c2bbfa30b170c22c42ab85f5ed64e33f654a
MRBeresford <125281117+MRBeresford@users.noreply.github.com>
2025-10-02 12:26:56 +0100

    DDCE-6989 remove sbt-accessibility-linter plugin (#233)
    * DDCE-6989 remove sbt-accessibility-linter plugin
    
    * Update .gitignore and remove .github folder
    
    * Update sbt-plugin, play-frontend-hmrc-play-30 and small update to govuk_wrapper.scala.html

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,13 +3,13 @@ import sbt.*
 
 object AppDependencies {
   val openHtmlVersion  = "1.1.31"
-  val bootstrapVersion = "9.19.0"
-  val mongoVersion     = "2.7.0"
+  val bootstrapVersion = "10.2.0"
+  val mongoVersion     = "2.9.0"
 
   val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"             %% "bootstrap-frontend-play-30" % bootstrapVersion,
     "uk.gov.hmrc.mongo"       %% "hmrc-mongo-play-30"         % mongoVersion,
-    "uk.gov.hmrc"             %% "play-frontend-hmrc-play-30" % "12.1.0",
+    "uk.gov.hmrc"             %% "play-frontend-hmrc-play-30" % "12.15.0",
     "uk.gov.hmrc"             %% "play-partials-play-30"      % "10.1.0",
     "uk.gov.hmrc"             %% "domain-play-30"             % "11.0.0",
     "io.github.openhtmltopdf" %  "openhtmltopdf-pdfbox"       % openHtmlVersion,
@@ -19,7 +19,7 @@ object AppDependencies {
     "io.github.openhtmltopdf" %  "openhtmltopdf-svg-support"  % openHtmlVersion
   )
 
-  val test: Seq[ModuleID]      = Seq(
+  private val test: Seq[ModuleID]      = Seq(
     "uk.gov.hmrc"             %% "bootstrap-test-play-30"  % bootstrapVersion,
     "uk.gov.hmrc.mongo"       %% "hmrc-mongo-test-play-30" % mongoVersion
   ).map(_ % Test)
