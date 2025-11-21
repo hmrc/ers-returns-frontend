ba827cca3895cb962b25a15e622e8afe4b44d21a
alisajadg191 <34246449+alisajadg191@users.noreply.github.com>
2025-11-21 11:24:35 +0000

    DDCE-7433: Added a check for correct file name.

--- public/javascripts/file-upload.js
+++ public/javascripts/file-upload.js
@@ -27,7 +27,8 @@ function checkFileName() {
     let actualFileName = fileUploadInput.files[0].name;
     let lowerName = actualFileName.toLowerCase();
         if (fileType.toLowerCase() === "csv".toLowerCase()) {
-            return {fileName: lowerName.endsWith(".csv"), fileNameLength: true, fileNameCharacters: true};
+            let expectedFileName = actualFileName === document.getElementsByTagName('label').item(0).getAttribute('data-file-name');
+            return {fileName: lowerName.endsWith(".csv") && expectedFileName, fileNameLength: true, fileNameCharacters: true};
         } else if (fileType.toLowerCase() === "ods".toLowerCase()) {
             return {fileName: lowerName.endsWith(".ods"), fileNameLength: actualFileName.length < 240, fileNameCharacters: actualFileName.match("[/^~\"|#?,\\]\\[£$&:@*\\\\+%{}<>\\/]|]") == null};
     } else {
