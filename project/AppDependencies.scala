5246b883ad2939b0ef1ea22659c6c445a3b1784a
Michael Sweeton <34985534+machinesleet@users.noreply.github.com>
2026-06-26 12:05:15 +0100

    DDCE-8672 play-frontend-hmrc upgrade, remove unused ocnfid and main template unused components

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -9,7 +9,7 @@ object AppDependencies {
   private val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"            %% "bootstrap-frontend-play-30" % bootstrapVersion,
     "uk.gov.hmrc.mongo"      %% "hmrc-mongo-play-30"         % mongoVersion,
-    "uk.gov.hmrc"            %% "play-frontend-hmrc-play-30" % "12.32.1",
+    "uk.gov.hmrc"            %% "play-frontend-hmrc-play-30" % "13.9.0",
     "uk.gov.hmrc"            %% "domain-play-30"             % "11.0.0",
     "io.github.openhtmltopdf" % "openhtmltopdf-pdfbox"       % openHtmlVersion,
     "commons-codec"           % "commons-codec"              % "1.21.0",
