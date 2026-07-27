9a7d5c6c4d010258872896566cfb0b40f7b9abaa
Mark Beresford <mark.beresford@digital.hmrc.gov.uk>
2026-07-27 15:28:29 +0100

    Formatting

--- app/utils/Retryable.scala
+++ app/utils/Retryable.scala
@@ -40,7 +40,7 @@ trait Retryable extends Logging {
     )(pToBreakLoop: A => Boolean)(implicit actorSystem: ActorSystem, ec: ExecutionContext): Future[A] = {
       val delay: FiniteDuration                                       = appConfig.retryDelay
       val scheduler: Scheduler                                        = actorSystem.getScheduler
-      val schemeRef                                            = maybeSchemeInfo.map(_.schemeRef).getOrElse("NOT DEFINED")
+      val schemeRef                                                   = maybeSchemeInfo.map(_.schemeRef).getOrElse("NOT DEFINED")
       def loop(count: Int = 0, previous: Option[A] = None): Future[A] = {
         logger.info(s"[$callingFunc][withRetry] Retrying call x$count, schemeRef: $schemeRef")
         if (count < maxTimes) {
@@ -53,7 +53,9 @@ trait Retryable extends Logging {
           }
         } else {
           // Logging "EXHAUSTED MAX NUMBER OF RETRIES" will cause alert to be triggered
-          logger.info(s"[$callingFunc][withRetry] EXHAUSTED MAX NUMBER OF RETRIES ($maxTimes times), schemeRef: $schemeRef")
+          logger.info(
+            s"[$callingFunc][withRetry] EXHAUSTED MAX NUMBER OF RETRIES ($maxTimes times), schemeRef: $schemeRef"
+          )
           throw LoopException(count, previous)
         }
       }
--- test/utils/RetryableSpec.scala
+++ test/utils/RetryableSpec.scala
@@ -37,20 +37,20 @@ import scala.concurrent.duration.SECONDS
 
 class RetryableSpec
     extends AnyWordSpecLike
-      with Matchers
-      with OptionValues
-      with MockitoSugar
-      with GuiceOneAppPerSuite
-      with ErsTestHelper
-      with LogCapturing
-      with ScalaFutures {
+    with Matchers
+    with OptionValues
+    with MockitoSugar
+    with GuiceOneAppPerSuite
+    with ErsTestHelper
+    with LogCapturing
+    with ScalaFutures {
 
   val retryTestLogger: Logger = Logger(classOf[RetryTest])
 
   class RetryTest extends Retryable {
     import scala.concurrent.duration._
 
-    override val logger: Logger = retryTestLogger
+    override val logger: Logger          = retryTestLogger
     val mockAppConfig: ApplicationConfig = mock[ApplicationConfig]
     when(mockAppConfig.retryDelay).thenReturn(1.millisecond)
 
@@ -64,9 +64,11 @@ class RetryableSpec
     val retryMock: RetryTestUtil = mock[RetryTestUtil]
   }
 
-  def generateExpectedRetryLogMessages(numberRetryLogs: Int,
-                                       appendMaxNumberRetiresMessage: Boolean = false,
-                                       schemeRef: String = "NOT DEFINED"): Seq[String] = {
+  def generateExpectedRetryLogMessages(
+    numberRetryLogs: Int,
+    appendMaxNumberRetiresMessage: Boolean = false,
+    schemeRef: String = "NOT DEFINED"
+  ): Seq[String] = {
     val retryLogMessages: Seq[String] = (0 until numberRetryLogs).map((retry: Int) =>
       s"[RetryableSpec][withRetry] Retrying call x$retry, schemeRef: $schemeRef"
     )
@@ -78,67 +80,78 @@ class RetryableSpec
     }
   }
 
-    "withRetry" should {
+  "withRetry" should {
 
-      "return the future data once the predicate has been fulfilled" in new RetryTest {
-        when(retryMock.f).thenReturn(Future.successful(true))
-        val expectedLogMessage = "[RetryableSpec][withRetry] Retrying call x0, schemeRef: NOT DEFINED"
-        withCaptureOfLoggingFrom(retryTestLogger) { captureEvents =>
-          val result: Boolean = retryMock.f.withRetry(5, callingFunc = "RetryableSpec")(b => b).futureValue
-          assert(result)
-          assert(captureEvents.exists(_.getMessage.contains(expectedLogMessage)))
-        }
-
-        verify(retryMock, times(1)).f
+    "return the future data once the predicate has been fulfilled" in new RetryTest {
+      when(retryMock.f).thenReturn(Future.successful(true))
+      val expectedLogMessage = "[RetryableSpec][withRetry] Retrying call x0, schemeRef: NOT DEFINED"
+      withCaptureOfLoggingFrom(retryTestLogger) { captureEvents =>
+        val result: Boolean = retryMock.f.withRetry(5, callingFunc = "RetryableSpec")(b => b).futureValue
+        assert(result)
+        assert(captureEvents.exists(_.getMessage.contains(expectedLogMessage)))
       }
 
-      "retry if the predicate is not fulfilled" in new RetryTest {
-        when(retryMock.f).thenReturn(
-          Future.successful(false),
-          Future.successful(false),
-          Future.successful(true)
-        )
-
-        withCaptureOfLoggingFrom(retryTestLogger) { captureEvents =>
-          val result: Boolean = retryMock.f.withRetry(5, callingFunc = "RetryableSpec")(b => b).futureValue
-          assert(result)
-          assert(captureEvents.map(_.getMessage) == generateExpectedRetryLogMessages(3))
-        }
-
-        verify(retryMock, times(3)).f
-      }
-
-      "retry up to a specified maximum number of times if the predicate is not fulfilled" in new RetryTest {
-        val schemeInfo: SchemeInfo = SchemeInfo("XA1100000000000", Instant.now, "1", "2016", "EMI", "EMI")
-        when(retryMock.f).thenReturn(
-          Future.successful(false),
-          Future.successful(false),
-          Future.successful(false),
-          Future.successful(false),
-          Future.successful(true)
-        )
-
-        withCaptureOfLoggingFrom(retryTestLogger) { captureEvents =>
-          val error: LoopException[Boolean] = intercept[LoopException[Boolean]] {
-            await(retryMock.f.withRetry(3, maybeSchemeInfo = Some(schemeInfo), callingFunc = "RetryableSpec")(b => b), 1, SECONDS)
-          }
-          assert(error == LoopException(3, Some(false)))
-          assert(error.getMessage == "Failed to meet predicate after retrying 3 times.")
-          assert(captureEvents.map(_.getMessage) == generateExpectedRetryLogMessages(4, schemeRef = "XA1100000000000", appendMaxNumberRetiresMessage = true))
-        }
-
-        verify(retryMock, times(3)).f
-      }
-
-      "return a LoopException if the predicate is never fulfilled" in new RetryTest {
-        when(retryMock.f).thenReturn(Future.successful(false))
-        val exception: LoopException[Boolean] = intercept[LoopException[Boolean]] {
-          await(retryMock.f.withRetry(1, callingFunc = "RetryableSpec")(b => b), 1, SECONDS)
-        }
-        exception.finalFutureData shouldBe Some(false)
-        exception.retryNumber     shouldBe 1
-        exception.getMessage      shouldBe s"Failed to meet predicate after retrying ${exception.retryNumber} times."
-        verify(retryMock, times(1)).f
-      }
+      verify(retryMock, times(1)).f
     }
+
+    "retry if the predicate is not fulfilled" in new RetryTest {
+      when(retryMock.f).thenReturn(
+        Future.successful(false),
+        Future.successful(false),
+        Future.successful(true)
+      )
+
+      withCaptureOfLoggingFrom(retryTestLogger) { captureEvents =>
+        val result: Boolean = retryMock.f.withRetry(5, callingFunc = "RetryableSpec")(b => b).futureValue
+        assert(result)
+        assert(captureEvents.map(_.getMessage) == generateExpectedRetryLogMessages(3))
+      }
+
+      verify(retryMock, times(3)).f
+    }
+
+    "retry up to a specified maximum number of times if the predicate is not fulfilled" in new RetryTest {
+      val schemeInfo: SchemeInfo = SchemeInfo("XA1100000000000", Instant.now, "1", "2016", "EMI", "EMI")
+      when(retryMock.f).thenReturn(
+        Future.successful(false),
+        Future.successful(false),
+        Future.successful(false),
+        Future.successful(false),
+        Future.successful(true)
+      )
+
+      withCaptureOfLoggingFrom(retryTestLogger) { captureEvents =>
+        val error: LoopException[Boolean] = intercept[LoopException[Boolean]] {
+          await(
+            retryMock.f.withRetry(3, maybeSchemeInfo = Some(schemeInfo), callingFunc = "RetryableSpec")(b => b),
+            1,
+            SECONDS
+          )
+        }
+        assert(error == LoopException(3, Some(false)))
+        assert(error.getMessage == "Failed to meet predicate after retrying 3 times.")
+        assert(
+          captureEvents.map(_.getMessage) == generateExpectedRetryLogMessages(
+            4,
+            schemeRef = "XA1100000000000",
+            appendMaxNumberRetiresMessage = true
+          )
+        )
+      }
+
+      verify(retryMock, times(3)).f
+    }
+
+    "return a LoopException if the predicate is never fulfilled" in new RetryTest {
+      when(retryMock.f).thenReturn(Future.successful(false))
+      val exception: LoopException[Boolean] = intercept[LoopException[Boolean]] {
+        await(retryMock.f.withRetry(1, callingFunc = "RetryableSpec")(b => b), 1, SECONDS)
+      }
+      exception.finalFutureData shouldBe Some(false)
+      exception.retryNumber     shouldBe 1
+      exception.getMessage      shouldBe s"Failed to meet predicate after retrying ${exception.retryNumber} times."
+      verify(retryMock, times(1)).f
+    }
+  }
+
 }
