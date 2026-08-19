40090b16b71fac38bcc30cc33d8d3cba2ba99049
amruta-lohote <203861386+amruta-lohote@users.noreply.github.com>
2026-08-19 13:22:37 +0100

    DDCE-7686- Implementin Error Page for File Naming Errors in ERS Returns Service

---
+++ app/controllers/ValidFileNameController.scala
@@ -0,0 +1,59 @@
+/*
+ * Copyright 2026 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers
+
+import config.ApplicationConfig
+import controllers.auth.AuthAction
+import models._
+import org.apache.pekko.actor.ActorSystem
+import play.api.Logging
+import play.api.i18n.{I18nSupport, Messages}
+import play.api.mvc._
+import services.FrontendSessionService
+import uk.gov.hmrc.play.bootstrap.frontend.controller.FrontendController
+import utils._
+
+import javax.inject.{Inject, Singleton}
+import scala.concurrent.ExecutionContext
+
+@Singleton
+class ValidFileNameController @Inject() (
+  val mcc: MessagesControllerComponents,
+  val sessionService: FrontendSessionService,
+  incorrectFileName: views.html.incorrect_file_name,
+  authAction: AuthAction
+)(implicit
+  val ec: ExecutionContext,
+  val ersUtil: ERSUtil,
+  val appConfig: ApplicationConfig,
+  val actorSystem: ActorSystem
+) extends FrontendController(mcc) with Retryable with I18nSupport with Logging with CacheHelper {
+
+  def getIncorrectFileNamePage(
+    requestObject: RequestObject
+  )(implicit request: RequestHeader, messages: Messages): Result =
+    BadRequest(
+      incorrectFileName(requestObject)(request, messages, appConfig)
+    )
+
+  def fileNameValidationFailure(): Action[AnyContent] = authAction.async { implicit request =>
+    for {
+      requestObject: RequestObject <- sessionService.fetch[RequestObject](ersUtil.ERS_REQUEST_OBJECT)
+    } yield getIncorrectFileNamePage(requestObject)
+  }
+
+}
---
+++ test/controllers/ValidFileNameControllerSpec.scala
@@ -0,0 +1,94 @@
+/*
+ * Copyright 2026 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers
+
+import config.ApplicationConfig
+import models._
+import org.apache.pekko.actor.ActorSystem
+import org.apache.pekko.stream.Materializer
+import org.mockito.ArgumentMatchers._
+import org.mockito.Mockito._
+import org.scalatest.BeforeAndAfterEach
+import org.scalatest.concurrent.ScalaFutures
+import org.scalatest.matchers.should.Matchers
+import org.scalatest.wordspec.AnyWordSpecLike
+import org.scalatestplus.play.guice.GuiceOneAppPerSuite
+import play.api.i18n
+import play.api.i18n.{MessagesApi, MessagesImpl}
+import play.api.mvc._
+import play.api.test.FakeRequest
+import play.api.test.Helpers._
+import utils.ErsTestHelper
+import utils.Fixtures.ersRequestObject
+import views.html.incorrect_file_name
+
+import scala.concurrent.{ExecutionContext, Future}
+
+class ValidFileNameControllerSpec
+    extends AnyWordSpecLike
+    with Matchers
+    with ErsTestHelper
+    with GuiceOneAppPerSuite
+    with BeforeAndAfterEach
+    with ScalaFutures {
+
+  val mockMCC: MessagesControllerComponents = DefaultMessagesControllerComponents(
+    messagesActionBuilder,
+    DefaultActionBuilder(stubBodyParser[AnyContent]())(ExecutionContext.global),
+    cc.parsers,
+    fakeApplication().injector.instanceOf[MessagesApi],
+    cc.langs,
+    cc.fileMimeTypes,
+    ExecutionContext.global
+  )
+
+  val incorrectFileNameView: incorrect_file_name = app.injector.instanceOf[incorrect_file_name]
+  implicit lazy val testMessages: MessagesImpl   = MessagesImpl(i18n.Lang("en"), mockMCC.messagesApi)
+  implicit lazy val mat: Materializer            = app.materializer
+  implicit val appConfig: ApplicationConfig      = app.injector.instanceOf[ApplicationConfig]
+  implicit val actorSystem: ActorSystem          = app.actorSystem
+
+  override def beforeEach(): Unit =
+    reset(mockErsUtil)
+
+  setAuthMocks()
+
+  "calling fileNameValidationFailure" should {
+
+    "return BAD_REQUEST and render incorrect file name page" in {
+
+      when(
+        mockSessionService.fetch[RequestObject](any())(any(), any())
+      ) thenReturn Future.successful(ersRequestObject)
+
+      val controller =
+        new ValidFileNameController(mockMCC, mockSessionService, incorrectFileNameView, testAuthAction)(
+          ec,
+          mockErsUtil,
+          appConfig,
+          actorSystem
+        )
+
+      val result = controller.fileNameValidationFailure().apply(FakeRequest("GET", ""))
+      status(result)        shouldBe BAD_REQUEST
+      contentAsString(result) should include(
+        testMessages("ers.incorrect_file_name.title")
+      )
+    }
+  }
+
+}
